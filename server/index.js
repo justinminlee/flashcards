b44b4e3648da0dca216d5e1d96d398359b9b6dc9
muujii1015 <munkhjargal.lkhagvasuren@griffithuni.edu.au>
2024-07-13 13:21:26 +1000

    added server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON in the request body
+
+// app.use(express.static(`${__dirname}/story/movies`));
+
+app.get('/users', async (req, res) => {
+    try {
+        const data = fs.readFileSync('data.json', 'utf8');
+        return res.send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+ 
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
