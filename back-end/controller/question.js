05db88de9afb2304151a8eb6fc3bdefc3a421557
Justin Lee <smsc19970205@gmail.com>
2024-07-13 17:26:01 +1000

    added openai api support

---
+++ back-end/controller/question.js
@@ -0,0 +1,49 @@
+const pdfParser = require('pdf-parse');
+const { OpenAI } = require("openai");
+const fs = require("fs");
+
+const openai = new OpenAI({
+    apiKey: "sk-None-6cG6sCXSevpKq6b1w168T3BlbkFJodr1gVnf866r9eTZNEq4"
+});
+
+let example_response = fs.readFileSync(`${__dirname}/structure.json`, 'utf-8');
+
+example_response = JSON.parse(example_response);
+
+const generateAnkiCards = async (req, res) => {
+  if (!req.files || !req.files.pdf) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  const pdfFile = req.files.pdf;
+  try {
+    const pdfData = await pdfParser(pdfFile.data);
+    const text = pdfData.text;
+
+
+    const prompt = `use the this JSON format ${JSON.stringify(example_response)} to create new json file for the following content ${text}. Do not miss any topics, make sure you describe and create quizzes for each top using the format.`
+
+    const messages = [
+        { role: 'user', content: prompt },
+        { role: 'system', content: `this is the structure for the response ${example_response.toString()}`},
+    ];
+
+    const completion = await openai.chat.completions.create({
+        response_format: { "type": "json_object" },
+        messages,
+        model: 'gpt-3.5-turbo',
+        temperature: 0
+      });
+    
+    const answer = completion.choices[0].message.content;
+    const id = completion.id;
+
+    console.log(answer)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error generating questions.');
+  }
+};
+
+module.exports = { generateAnkiCards };
--- back-end/index.js
+++ back-end/index.js
@@ -1,26 +1,44 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const { OpenAI } = require("openai");
+const fileUpload = require('express-fileupload');
+const pdfParser = require('pdf-parse');
+
+const openai = new OpenAI({
+  apiKey: "sk-proj-UMDEM0WkAQ7WP2yUNGa9T3BlbkFJRneYtDWmQ6eA0Tammv5O",
+});
+
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 // Enable CORS
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON in the request body
+
+const { generateAnkiCards } = require('./controller/question');
+
+app.post('/upload', generateAnkiCards);
 
 // app.use(express.static(`${__dirname}/story/movies`));
 
 app.get('/upload_file', async (req, res) => {
     try {
-        const data = fs.readFileSync('data.json', 'utf8');
-        return res.send(data);
+        console.log(data);
+        const data = fs.readFileSync('data.json', 'utf-8');
+        return res.send(data).status(200);
     } catch (err) {
         console.error(err);
-        return res.status(500).send('Internal Server Error');
+        return res.send(data);
     }
 });
  
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
